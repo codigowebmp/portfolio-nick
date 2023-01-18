6475143a2955399e6c03758fc0c9361ef8d08a9c
Javi970 <javierpaivag@gmail.com>
2023-01-18 12:33:40 -0300

    fix form

--- script.js
+++ script.js
@@ -39,3 +39,21 @@ function efectoHabilidades(){
 window.onscroll = function(){
     efectoHabilidades();
 } 
+const $form = document.querySelector("#form")
+$form.addEventListener("submit",handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form =new FormData(this)
+    const response= await fetch(this.action,{
+        method : this.method,
+        body:form,
+        headers:{
+            "Accept":"application/json"
+        }
+    })
+    if(response.ok){
+        this.reset()
+        alert("Gracias por contactarme, te escribiré pornto!")
+    }
+}

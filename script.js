c4e24940d0967e3042ec8333a1253db500c2e5bc
Javi970 <javierpaivag@gmail.com>
2023-04-17 19:32:39 -0300

    update cv

--- script.js
+++ script.js
@@ -1,59 +1,61 @@
 let menuVisible = false;
 //Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
-        menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
-        menuVisible = true;
-    }
-}
-
-function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
+function mostrarOcultarMenu() {
+  if (menuVisible) {
     document.getElementById("nav").classList = "";
     menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
+}
+
+function seleccionar() {
+  //oculto el menu una vez que selecciono una opcion
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
 }
 //Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
-    }
+function efectoHabilidades() {
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progreso");
+    habilidades[0].classList.add("javascript");
+    habilidades[1].classList.add("htmlcss");
+    habilidades[2].classList.add("react");
+    habilidades[3].classList.add("nodejs");
+    habilidades[4].classList.add("redux");
+    habilidades[5].classList.add("express");
+    habilidades[6].classList.add("postgresql");
+    habilidades[7].classList.add("git");
+    habilidades[8].classList.add("tailwindcss");
+    habilidades[9].classList.add("typescript");
+    habilidades[10].classList.add("mongodb");
+    habilidades[11].classList.add("bootstrap");
+  }
 }
 
-
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-} 
-const $form = document.querySelector("#form")
-$form.addEventListener("submit",handleSubmit)
+window.onscroll = function () {
+  efectoHabilidades();
+};
+const $form = document.querySelector("#form");
+$form.addEventListener("submit", handleSubmit);
 
-async function handleSubmit(event){
-    event.preventDefault()
-    const form =new FormData(this)
-    const response= await fetch(this.action,{
-        method : this.method,
-        body:form,
-        headers:{
-            "Accept":"application/json"
-        }
-    })
-    if(response.ok){
-        this.reset()
-        alert("Gracias por contactarme, te escribiré pornto!")
-    }
+async function handleSubmit(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    this.reset();
+    alert("Gracias por contactarme, te escribiré pornto!");
+  }
 }
